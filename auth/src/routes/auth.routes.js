7d41ffc7859578c8895b2aac55056d25bcb33f7a
Abhishek Jha <abhibj2003@gmail.com>
2026-02-14 04:20:59 +0530

    done

--- auth/src/routes/auth.routes.js
+++ auth/src/routes/auth.routes.js
@@ -50,7 +50,30 @@ authRouter.get("/get-me", async (req,res)=>{
   
 });
 
+authRouter.post('/login', async (req,res)=>{
+    const {email,password} = req.body
+    const user = await userModel.findOne({email})
+    if(!user){
+        return res.status(404).json({
+            message : "user not found"
+        })
+    }
 
+    const hash = crypto.createHash('sha256').update(password).digest('hex')
+
+    const isPasswordValid = hash === user.password
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message : "invalid password"
+        })
+    }
+
+    const token =jwt.sign({
+        id: user._id,
+    },process.env.JWT_SECRET,{expiresIn:"1h"})
+
+    res.cookie("token", token)
+})
 
 
 module.exports = authRouter;
